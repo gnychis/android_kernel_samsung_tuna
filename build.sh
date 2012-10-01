7cc05dd5017ae4d8cb9cb3d6d6bb538493144801
George Nychis <gnychis@gmail.com>
2012-10-01 03:44:10 -0400

    adding ftdio support and rt2x00 support

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export ARCH=arm
-export CROSS_COMPILE=`readlink -f ../linaro-android-toolchain`/bin/arm-eabi-
+#export CROSS_COMPILE=`readlink -f ../linaro-android-toolchain`/bin/arm-eabi-
+export CROSS_COMPILE=`readlink -f ../os/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3`/bin/arm-eabi-
 make clean
 make cyanogenmod_tuna_awmon_defconfig
 make -j2 
--- noclean_build.sh
+++ noclean_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export ARCH=arm
-export CROSS_COMPILE=`readlink -f ../linaro-android-toolchain`/bin/arm-eabi-
+#export CROSS_COMPILE=`readlink -f ../linaro-android-toolchain`/bin/arm-eabi-
+export CROSS_COMPILE=`readlink -f ../os/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3`/bin/arm-eabi-
 make -j2 
 ./package.sh
