6704df4296a03ff0d9854861aa6d89dad74342e6
George Nychis <gnychis@gmail.com>
2012-10-01 02:14:52 -0400

    adding in rt2x00 usb support to the kernel

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 export ARCH=arm
 export CROSS_COMPILE=`readlink -f ../linaro-android-toolchain`/bin/arm-eabi-
 make clean
-make cyanogenmod_tuna_defconfig
+make cyanogenmod_tuna_awmon_defconfig
 make -j2 
 ./package.sh
