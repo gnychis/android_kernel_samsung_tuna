760ae5534ce7b0a522602f5d427fbb05ff8f52d4
George Nychis <gnychis@gmail.com>
2012-10-05 14:15:08 -0700

    now building the wireless support as a module and loading it at init

--- noclean_build.sh
+++ noclean_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export ARCH=arm
 #export CROSS_COMPILE=`readlink -f ../linaro-android-toolchain`/bin/arm-eabi-
-export CROSS_COMPILE=`readlink -f ../os/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6`/bin/arm-linux-androideabi-
+export CROSS_COMPILE=`readlink -f ../os/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6`/bin/arm-eabi-
+#export CROSS_COMPILE=`readlink -f ../os/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6`/bin/arm-linux-androideabi-
 make -j2 
 ./package.sh
