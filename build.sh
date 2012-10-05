dcd4e9b2d90784d486a44052f1ba49ec1ed9457d
George Nychis <gnychis@gmail.com>
2012-10-05 12:47:40 -0700

    properly building in the monitor kernel module

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export ARCH=arm
-#export CROSS_COMPILE=`readlink -f ../linaro-android-toolchain`/bin/arm-eabi-
 export CROSS_COMPILE=`readlink -f ../os/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6`/bin/arm-linux-androideabi-
 make clean
 make cyanogenmod_tuna_awmon_defconfig
