6060f2ec09e5ffdd2caa55c9e5b4d133f281c7b9
George Nychis <gnychis@gmail.com>
2012-10-05 10:56:06 -0700

    changing the package to remove the ramdiskaddr

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ rm -f package_tree/boot.img
 find . -name "*.ko" -exec cp {} package_tree/system/lib/modules \;
 ./mkbootfs initramfs | gzip > ramdisk.gz
 cp arch/arm/boot/zImage .
-./mkbootimg --kernel zImage --ramdisk ramdisk.gz -o boot.img --base 0x80000000 --pagesize 2048 --ramdiskaddr 0x81500000
+./mkbootimg --kernel zImage --ramdisk ramdisk.gz -o boot.img --base 0x80000000 --pagesize 2048
 cp boot.img package_tree
 rm zImage
 cd package_tree
