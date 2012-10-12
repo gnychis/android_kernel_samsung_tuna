60a63e07db69e28833c9384494a0d4a1b148c513
George Nychis <gnychis@gmail.com>
2012-10-12 16:04:17 -0700

    adding in more modules to insert

--- package.sh
+++ package.sh
@@ -4,9 +4,11 @@ rm -f package_tree/system/lib/modules/*
 rm -f package_tree/boot.img
 find . -name "*.ko" -exec cp {} package_tree/system/lib/modules \;
 #cd ../compat-wireless-3.6-rc7-1
-#./make.sh
-#cd ../galaxynexus-cm10-kernel
-find ../compat-wireless-3.6-rc7-1/ -name "*.ko" -exec cp {} package_tree/system/lib/modules/ \;
+cd ../compat-wireless-3.0.9-1
+./make.sh
+cd ../galaxynexus-cm10-kernel
+#find ../compat-wireless-3.6-rc7-1/ -name "*.ko" -exec cp {} package_tree/system/lib/modules/ \;
+find ../compat-wireless-3.0.9-1/ -name "*.ko" -exec cp {} package_tree/system/lib/modules/ \;
 ./mkbootfs initramfs | gzip > ramdisk.gz
 cp arch/arm/boot/zImage .
 ./mkbootimg --kernel zImage --ramdisk ramdisk.gz -o boot.img --base 0x80000000 --pagesize 2048
